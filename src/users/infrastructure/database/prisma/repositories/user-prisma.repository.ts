ae9e05c4b8f05883de7cfff916e1f1dd95bff8ae
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2025-11-29 15:35:14 -0300

    create user-prisma repository

--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -24,8 +24,46 @@ export class UserPrismaRepository implements UserRepository {
   emailExist(email: string): Promise<void> {
     throw new Error('Method not implemented.')
   }
-  search(props: SearchParams): Promise<SearchResults> {
-    throw new Error('Method not implemented.')
+  async search(props: SearchParams): Promise<SearchResults> {
+    const sortable = this.sortableFields?.includes(props.sort) || false
+    const orderByField = sortable ? props.sort : 'createdAt'
+    const orderByDir = sortable ? props.sortDir : 'desc'
+
+    const count = await this.prismaService.user.count({
+      ...(props.filter && {
+        where: {
+          name: {
+            contains: props.filter,
+            mode: 'insensitive',
+          },
+        },
+      }),
+    })
+    const models = await this.prismaService.user.findMany({
+      ...(props.filter && {
+        where: {
+          name: {
+            contains: props.filter,
+            mode: 'insensitive',
+          },
+        },
+      }),
+      orderBy: {
+        [orderByField]: orderByDir,
+      },
+      skip: props.page && props.page > 0 ? (props.page - 1) * props.perPage : 1,
+      take: props.perPage && props.perPage > 0 ? props.perPage : 15,
+    })
+
+    return new SearchResults({
+      items: models.map(model => UserModelMapper.toEntity(model)),
+      total: count,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: orderByField,
+      sortDir: orderByDir,
+      filter: props.filter,
+    })
   }
   insert(entities: UserEntity): Promise<void> {
     throw new Error('Method not implemented.')
