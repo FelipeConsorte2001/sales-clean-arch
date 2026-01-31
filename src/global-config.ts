d756b8f149fd6e04f590ffcd15c210e19eb190d3
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-01-31 15:12:58 -0300

    feat: aplly customized testes

--- src/global-config.ts
+++ src/global-config.ts
@@ -4,6 +4,10 @@ import {
   ValidationPipe,
 } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
+import { ConflictErrorFilter } from './shared/infrastructure/exception-filters/conflict-error/conflict-error.filter'
+import { InvalidCredentialsErrorFilter } from './shared/infrastructure/exception-filters/invalid-credentials-error/invalid-credentials-error.filter'
+import { InvalidPasswordErrorFilter } from './shared/infrastructure/exception-filters/invalid-password-error/invalid-password-error.filter'
+import { NotFoundErrorFilter } from './shared/infrastructure/exception-filters/not-found-error/not-found-error.filter'
 import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor'
 
 export function applyGlobalConfig(app: INestApplication) {
@@ -18,5 +22,11 @@ export function applyGlobalConfig(app: INestApplication) {
   app.useGlobalInterceptors(
     new WrapperDataInterceptor(),
     new ClassSerializerInterceptor(app.get(Reflector)),
+  )
+  app.useGlobalFilters(
+    new ConflictErrorFilter(),
+    new NotFoundErrorFilter(),
+    new InvalidPasswordErrorFilter(),
+    new InvalidCredentialsErrorFilter(),
   )
 }
