4bc66c66cac8f62b8866168fdd88735ce4d2bfa0
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-01-31 15:12:40 -0300

    feat: create custonized exception for invalid-password, confict erro, not found erros and their tests e2e

---
+++ src/shared/infrastructure/exception-filters/invalid-credentials-error/__tests__/e2e/invalid-credentials-error.filter.e2e-spec.ts
@@ -0,0 +1,38 @@
+import { InvalidCredentialsError } from '@/shared/application/erros/invalid-credentials-error'
+import { Controller, Get, INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import request from 'supertest'
+import { InvalidCredentialsErrorFilter } from '../../invalid-credentials-error.filter'
+
+@Controller('stub')
+class StubController {
+  @Get()
+  index() {
+    throw new InvalidCredentialsError('Invalid credentials')
+  }
+}
+describe('InvalidCredentialsErrorFilter', () => {
+  let app: INestApplication
+  let module: TestingModule
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [StubController],
+    }).compile()
+    app = module.createNestApplication()
+    app.useGlobalFilters(new InvalidCredentialsErrorFilter())
+    await app.init()
+  })
+
+  it('should be defined', async () => {
+    expect(new InvalidCredentialsErrorFilter()).toBeDefined()
+  })
+
+  it('should catch a InvalidPasswordError', () => {
+    return request(app.getHttpServer()).get('/stub').expect(400).expect({
+      statusCode: 400,
+      error: 'Bad Request',
+      message: 'Invalid credentials',
+    })
+  })
+})
