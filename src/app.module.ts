cea9acdaeb3f714afe8812f4a4d163bbd04a4251
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-03-07 14:08:22 -0300

    feat: Implement auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { AuthModule } from './auth/infrastructure/auth.module'
 import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
-  imports: [EnvConfigModule.forRoot(), UsersModule, DatabaseModule],
+  imports: [EnvConfigModule.forRoot(), UsersModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [],
 })
