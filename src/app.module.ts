87ad1fcf869100a81137cc7461e7d1127a233b7c
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-03-28 13:18:51 -0300

    feat: create category model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/infrastructure/auth.module'
+import { CategoryModule } from './category/category.module'
+import { CategoryService } from './category/infrastructure/category.service'
 import { DatabaseModule } from './shared/infrastructure/database/database.module'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
-  imports: [EnvConfigModule.forRoot(), UsersModule, DatabaseModule, AuthModule],
+  imports: [EnvConfigModule.forRoot(), UsersModule, DatabaseModule, AuthModule, CategoryModule],
   controllers: [],
-  providers: [],
+  providers: [CategoryService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/category/category.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { CategoryController } from './infrastructure/category.controller';
+
+@Module({
+  controllers: [CategoryController]
+})
+export class CategoryModule { }
