2a3b81cd693ae04ca2ea70e4b577558100dc1c4f
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2026-03-28 13:19:10 -0300

    feat: create category migration

---
+++ src/shared/infrastructure/database/prisma/migrations/20260328152125/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "category" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
